b1aaaba2332f11baceb51a98fec92a6dfb0b4c5c
Francisco Alvarez <41089629+falvarezb@users.noreply.github.com>
2018-12-02 20:30:37 +0000

    goodland electricity

--- src/main/java/fjab/Main.java
+++ src/main/java/fjab/Main.java
@@ -1,17 +1,52 @@
 package fjab;
 
-import java.util.ArrayList;
+import java.io.BufferedReader;
+import java.io.FileReader;
 import java.util.Arrays;
 import java.util.List;
 
 public class Main {
 
-    public static void main(String[] args) {
+    static String readFile() throws Exception{
+        try(BufferedReader br = new BufferedReader(new FileReader("src/main/resources/testCase"))) {
+            StringBuilder sb = new StringBuilder();
+            String line = br.readLine();
+
+            while (line != null) {
+                sb.append(line);
+                sb.append(System.lineSeparator());
+                line = br.readLine();
+            }
+            return sb.toString();
+        }
+    }
+
+    public static void main(String[] args) throws Exception{
 //        int[] arr = {1,5,3,4,2};
 //        System.out.println(pairs(2, arr));
 
-        List<Integer> arr = new ArrayList<>(Arrays.asList(0,0,2,0));
-        System.out.println(balancedSums(arr));
+//        List<Integer> arr = new ArrayList<>(Arrays.asList(0,0,2,0));
+//        System.out.println(balancedSums(arr));
+
+        //int[] arr = {1,1,0,0,0,1,1,1,1,1};
+        //int[] arr = {0,1,1,1,1,0};
+        //int[] arr = {0, 1, 0, 0, 0, 1, 1, 1, 1, 1};
+
+        int[] arr = new int[100000];
+        String[] n = readFile().split(",");
+        for(int j=0; j<n.length-1; j++){
+            try{
+                arr[j] = Integer.parseInt(n[j]);
+            }
+            catch (Exception ex){
+                System.out.println(j);
+                throw ex;
+            }
+        }
+        arr[99999] = 1;
+
+        //int[] arr = Arrays.stream(readFile().split(",")).mapToInt(i -> Integer.parseInt(i)).toArray();
+        System.out.println(pylons(4,arr));
     }
 
     static int pairs(int k, int[] arr) {
@@ -56,6 +91,53 @@ public class Main {
         for(int i=0; i<n-1; i++){
             for(int j=i+1; j<n; j++){
                 if((ar[i] + ar[j])%k == 0) counter++;
+            }
+        }
+        return counter;
+    }
+
+    static int pylons(int k, int[] arr) {
+
+        int lastPlantPosition = -k;
+        int leftMostTownInTheDark = arr.length;
+        int lastCandidateForPlant = arr.length;
+        int counter = 0;
+
+        for(int i=0; i<arr.length; i++){
+
+            boolean isCurrentTownIlluminated = i - lastPlantPosition < k;
+            boolean canBuildPlantInCurrentTown = arr[i] == 1;
+            boolean canBuildPlantInPreviousTown = lastCandidateForPlant < i;
+            boolean isLastTown = i == arr.length-1;
+
+            if(!isCurrentTownIlluminated) { //current town is not illuminated
+                if(i < leftMostTownInTheDark) //is current town the left most town in the dark
+                    leftMostTownInTheDark = i;
+            }
+
+            int distanceToLeftMostTownInDark = i - leftMostTownInTheDark;
+
+            if(canBuildPlantInCurrentTown && (distanceToLeftMostTownInDark == k-1 || isLastTown)){
+                lastPlantPosition = i;
+                leftMostTownInTheDark = lastPlantPosition + k;
+                lastCandidateForPlant = arr.length;
+                counter++;
+                System.out.println(lastPlantPosition);
+            }
+            else if(distanceToLeftMostTownInDark == k-1 || isLastTown){
+                if(canBuildPlantInPreviousTown){
+                    lastPlantPosition = lastCandidateForPlant;
+                    lastCandidateForPlant = arr.length;
+                    leftMostTownInTheDark = lastPlantPosition + k;
+                    counter++;
+                    System.out.println(lastPlantPosition);
+                }
+                else if (!isCurrentTownIlluminated){
+                    return -1;
+                }
+            }
+            else if(canBuildPlantInCurrentTown){
+                lastCandidateForPlant = i;
             }
         }
         return counter;
--- src/test/scala/fjab/util/Utils.scala
+++ src/test/scala/fjab/util/Utils.scala
@@ -2,6 +2,8 @@ package fjab.util
 
 import org.scalatest.FunSuite
 
+import scala.annotation.tailrec
+
 class Utils extends FunSuite{
 
   def xIsPowerOfy(x: BigInt, y: BigInt): Boolean =
@@ -15,6 +17,57 @@ class Utils extends FunSuite{
   def numberGenerator(a: Int, b: Int): List[Int] =
     if(a == b) List(a)
     else a :: numberGenerator(a + 1, b)
+
+
+  def highestFrequencyOfOccurrences[T](arr: Array[T]) = arr.groupBy(identity).mapValues(_.length).maxBy(_._2)
+  def highestFrequencyOfOccurrences[T,U](arr: Array[T], f: T => U) = arr.groupBy(f).mapValues(_.length).maxBy(_._2)
+
+  def decimalRepresentation(n: BigInt): List[Int] = {
+    val q = n/10
+    val r = n%10
+    if(q == 0) List(r.intValue())
+    else r.intValue() :: decimalRepresentation(q)
+  }
+
+  def decimalRepresentationTailRecursive(n: BigInt): List[Int] = {
+
+    @tailrec
+    def acc(n: BigInt, representation: List[Int]): List[Int] = {
+      val q = n/10
+      val r = n%10
+      if(q == 0) r.intValue() :: representation
+      else acc(q, r.intValue() :: representation)
+    }
+
+    acc(n, Nil)
+  }
+
+  /**
+    * Factorial calculation using dynamic programming (with bottom-up memoisation)
+    */
+  def factorialDP(n: Int): BigInt = {
+
+    val intermediateResults = new Array[BigInt](n+1)
+    intermediateResults(0) = 1
+
+    for(i <- Range(1,n+1)){
+      intermediateResults(i) = i * intermediateResults(i-1)
+    }
+
+    intermediateResults(n)
+  }
+
+  test("decimal representation of 9012 is [9,0,1,2]"){
+    assert(decimalRepresentationTailRecursive(BigInt(9012)) == List(9,0,1,2))
+  }
+
+  test("in array [a,a,a,b,b,c], most frequent element is a with 3 occurrences"){
+    assert(highestFrequencyOfOccurrences(Array("a","a","a","b","b","c")) == ("a",3))
+  }
+
+  test("in array [1,2,3,4,5], most frequent element is 'odd' with 5 occurrences"){
+    assert(highestFrequencyOfOccurrences(Array(1,2,3,4,5), (n:Int) => if(n%2==0) "EVEN" else "ODD") == ("ODD",3))
+  }
 
   test("100 is power of 10"){
     assert(xIsPowerOfy(100,10))

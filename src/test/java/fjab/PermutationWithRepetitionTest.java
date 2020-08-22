7b53dd98459b3e32417b788b7cd6d6e15ba1c878
Francisco Alvarez <41089629+falvarezb@users.noreply.github.com>
2020-08-22 14:20:38 +0100

    test refactoring to use jqwik

--- src/test/java/fjab/PermutationWithRepetitionTest.java
+++ src/test/java/fjab/PermutationWithRepetitionTest.java
@@ -1,31 +1,34 @@
 package fjab;
 
-import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Nested;
-import org.junit.jupiter.api.Test;
+import net.jqwik.api.*;
+import net.jqwik.api.constraints.IntRange;
 
 import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 
 import static fjab.PermutationWithRepetition.allPermutations;
 import static fjab.PermutationWithRepetition.randomPermutation;
-import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
+@Label("PermutationWithRepetitionTest")
 class PermutationWithRepetitionTest {
 
-  @Nested
-  @DisplayName("calculation of all permutations")
+  @Group
+  @Label("test of 'allPermutations'")
   class AllPermutations {
-    @Test
-    @DisplayName("n=2, r=5")
-    public void testPermutation2_5() {
-      List<List<Integer>> result = allPermutations(Arrays.asList(0, 1), 5);
-      assertEquals(32, result.size());
+    @Property
+    @Label("check of the amount of permutations for different combinations of 'n' and 'r'")
+    public void testAmountPermutations(@ForAll @IntRange(min=1, max=5) int n, @ForAll @IntRange(min=1, max=5) int r) {
+      List<Integer> population = IntStream.range(0,n).boxed().collect(Collectors.toList());
+      List<List<Integer>> result = allPermutations(population, r);
+      assertEquals((int) Math.pow(n, r), result.size());
     }
 
-    @Test
-    @DisplayName("n=2, r=2")
+    @Example
+    @Label("check of the permutations corresponding to n=2, r=2")
     public void testPermutation2_2() {
       List<List<Integer>> result = allPermutations(Arrays.asList(0, 1), 2);
 
@@ -34,21 +37,23 @@ class PermutationWithRepetitionTest {
     }
   }
 
-  @Nested
-  @DisplayName("calculation of a random permutation")
+  @Group
+  @Label("test of 'randomPermutation'")
   class RandomPermutation {
-    @Test
-    @DisplayName("n=2, r=5")
-    public void testPermutation2_5() {
-      List<Integer> result = randomPermutation(Arrays.asList(0, 1), 5);
-      assertThat(allPermutations(Arrays.asList(0, 1), 5)).contains(result);
+    @Property
+    @Label("checking 4 random permutations for n=2, r=2")
+    public void testPermutation2_2(@ForAll @IntRange(min=1, max=4) int repetitions) {
+      List<Integer> population = Arrays.asList(0, 1);
+      int r = 2;
+      assertThat(allPermutations(population, r)).contains(randomPermutation(population, r));
     }
 
-    @Test
-    @DisplayName("n=2, r=2")
-    public void testPermutation2_2() {
-      List<Integer> result = randomPermutation(Arrays.asList(0, 1), 2);
-      assertThat(allPermutations(Arrays.asList(0, 1), 2)).contains(result);
+    @Property
+    @Label("checking 32 random permutations for n=2, r=5")
+    public void testPermutation2_5(@ForAll @IntRange(min=1, max=32) int repetitions) {
+      List<Integer> population = Arrays.asList(0, 1);
+      int r = 5;
+      assertThat(allPermutations(population, r)).contains(randomPermutation(population, r));
     }
   }
 

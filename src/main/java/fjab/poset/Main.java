70105de0cf3e4c4c4469dee7447efff764d5936f
Francisco Alvarez <41089629+falvarezb@users.noreply.github.com>
2020-09-05 18:05:20 +0100

    removing original implementation of Poset

--- src/main/java/fjab/poset/Main.java
+++ src/main/java/fjab/poset/Main.java
@@ -4,12 +4,13 @@ import fjab.poset.error.PosetException;
 
 import java.io.IOException;
 import java.nio.file.Paths;
+import java.util.Arrays;
 
 public class Main {
 
   public static void main(String[] args) throws IOException, PosetException {
 
-    Poset poset = new Poset(PosetUtil.buildBinaryRelationsFromFile(Paths.get("src/main/resources/poset1.txt")));
+    Poset2<String> poset = new Poset2<>(Arrays.asList("a", "b", "c", "d"), PosetUtil.buildBinaryRelationsFromFile(Paths.get("src/main/resources/poset1.txt")));
     System.out.println(poset.toString());
 
   }

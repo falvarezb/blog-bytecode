61577b08fc5bf9e9c052b54050ea27513a1860ca
Francisco Alvarez <41089629+falvarezb@users.noreply.github.com>
2021-02-25 22:38:07 +0000

    sum

--- src/main/scala/fjabs/Sum.scala
+++ src/main/scala/fjabs/Sum.scala
@@ -1,28 +1,64 @@
 package fjabs
 
-//A monoid represents types that can be summed
-trait Monoid[A] {
+//A semigroup represents types that can be summed
+trait Semigroup[A] {
   def combine(a1: A, a2: A): A
+}
+
+object Semigroup {
+  def apply[A: Semigroup]: Semigroup[A] = implicitly[Semigroup[A]]
+
+  implicit val intSemigroup = new Semigroup[Int] {
+    override def combine(a1: Int, a2: Int): Int = a1 + a2
+  }
+
+  implicit val stringSemigroup = new Semigroup[String] {
+    override def combine(a1: String, a2: String): String = a1 + a2
+  }
+}
+
+//A monoid is a semigroup with an empty value
+trait Monoid[A] extends Semigroup[A] {
   def empty: A //'empty' is the identity for the combine operation
 }
+final case class Pair[A, B](first: A, second: B)
 
 object Monoid {
 
+  import Semigroup._
   def apply[A: Monoid]: Monoid[A] = implicitly[Monoid[A]]
 
-  implicit val intMonoid = new Monoid[Int] {
-    override def combine(a1: Int, a2: Int): Int = a1 + a2
+  implicit def intMonoid: Monoid[Int] = new Monoid[Int] {
+    override def combine(a1: Int, a2: Int): Int = intSemigroup.combine(a1, a2) //implicit derivation results in an infinitely recursive call
     override def empty: Int = 0
   }
 
-  implicit val stringMonoid = new Monoid[String] {
-    override def combine(a1: String, a2: String): String = a1 + a2
+  implicit def stringMonoid: Monoid[String] = new Monoid[String] {
+    override def combine(a1: String, a2: String): String = stringSemigroup.combine(a1, a2)
     override def empty: String = ""
   }
 
-  val multiplicativeMonoid = new Monoid[Int] {
-    override def combine(a1: Int, a2: Int): Int = a1 * a2
-    override def empty: Int = 1
+//  implicit def multiplicativeMonoid: Monoid[Int] = new Monoid[Int] {
+//    override def combine(a1: Int, a2: Int): Int = a1 * a2
+//    override def empty: Int = 1
+//  }
+
+  //for any Semigroup[A], there is a Monoid[Option[A]]
+  implicit def optionInstance[A: Semigroup]: Monoid[Option[A]] = new Monoid[Option[A]] {
+    override def empty: Option[A] = None
+    override def combine(a1: Option[A], a2: Option[A]): Option[A] = a1 match {
+      case Some(value1) => a2 match {
+        case Some(value2) => Some(Semigroup[A].combine(value1, value2))
+        case None => a1
+      }
+      case None => a2
+    }
+  }
+
+  //typeclass derivation
+  implicit def tupleToPairInstance[A: Monoid, B: Monoid]: Monoid[Pair[A,B]] = new Monoid[Pair[A,B]] {
+    override def combine(a: Pair[A, B], b: Pair[A, B]): Pair[A, B] = Pair(Monoid[A].combine(a.first, b.first), Monoid[B].combine(a.second, b.second))
+    override def empty: Pair[A, B] = Pair(Monoid[A].empty, Monoid[B].empty)
   }
 }
 
@@ -33,7 +69,9 @@ object Sum extends App {
 
   println(sum(List(1,2,3,4)))
   println(sum(List("1","2","3")))
-  println(sum(List(1,2,3,4))(Monoid.multiplicativeMonoid))
+  //println(sum(List(1,2,3,4))(Monoid.multiplicativeMonoid))
+  println(sum(List(Pair(1,"2"), Pair(3,"4"))))
+  println(sum(List(Option(2), Option(3), None)))
 
 }
 
